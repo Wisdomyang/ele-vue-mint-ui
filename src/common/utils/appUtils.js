1d99b2bdc7ea84526df026099c246904840841a3
wuzhouyang <wuzhouyang2015@163.com>
2018-09-24 00:50:10 +0800

    新增一些工具函数

--- src/common/utils/appUtils.js
+++ src/common/utils/appUtils.js
@@ -1,43 +1,99 @@
 
 export const appUtils = {
-
-    deepCopy(o){
-	    if (o instanceof Array) {
-	        var n = [];
-	        for (var i = 0; i < o.length; ++i) {
-	            n[i] = this.deepCopy(o[i]);
-	        }
-	        return n;
-	    }else if(o instanceof Function || o instanceof RegExp){
+    // 深拷贝
+    deepCopy(o) {
+        if (this.preciseJudgmentType(o) === 'Array') {
+            let n = [];
+            for (let item of o) {
+                n.push(this.deepCopy(item));
+            }
+            return n;
+        } else if (this.preciseJudgmentType(o) === 'Object') {
+            let n = {};
+            for (let key in o) {
+                n[key] = this.deepCopy(o[key]);
+            }
+            return n;
+        } else {
             return o;
-        } else if (o instanceof Object) {
-	        var n = {}
-	        for (var i in o) {
-	            n[i] = this.deepCopy(o[i]);
-	        }
-	        return n;
-	    } else {
-	        return o;
-	    }
-	},
+        }
+    },
+    // 精确判断类型
+    preciseJudgmentType(val) {
+        switch (Object.prototype.toString.call(val)) {
+            case "[object String]":
+                return 'String';
+                break;
+            case "[object Number]":
+                return 'Number';
+                break;
+            case "[object Boolean]":
+                return 'Boolean';
+                break;
+            case "[object Undefined]":
+                return 'Undefined';
+                break;
+            case "[object Symbol]":
+                return 'Symbol';
+                break;
+            case "[object Object]":
+                return 'Object';
+                break;
+            case "[object Null]":
+                return 'Null';
+                break;
+            case "[object Array]":
+                return 'Array';
+                break;
+            case "[object Date]":
+                return 'Date';
+                break;
+            case "[object RegExp]":
+                return 'RegExp';
+                break;
+            case "[object Function]":
+                return 'Function';
+                break;
+        }
+    },
+    
+    sortByKey(key, param = 'up') {
+        return function (obj1, obj2) {
+            var value1 = obj1[key]
+            var value2 = obj2[key]
+            if (value1 < value2) {
+                if (param === 'up') {
+                    return -1;
+                } else {
+                    return 1;
+                }
+            } else if (value1 > value2) {
+                if (param === 'up') {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            } else {
+                return 0;
+            }
+        }
+    },
 
-    goBack(){
+    // 生成uuid
+    getUniqueId() {
+        let d = Date.now();
+        let uuid = 'xxxxxxxx-xxxx-xxxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            let r = (d + Math.random() * 16) % 16 | 0;
+            return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+        });
+        return uuid;
+    },
+
+    goBack() {
         window.isBack = true;
         window.history.back();
-    },
+    }
 
-    
-    // 生成uuid
-    getUniqueId(){
-        var d = new Date().getTime();
-        var uuid = 'xxxxxxxx-xxxx-xxxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-            var r = (d + Math.random()*16)%16 | 0;
-            d = Math.floor(d/16);
-            return (c =='x' ? r : (r&0x3|0x8)).toString(16);
-        });
-        return uuid;		
-    },
 
-    
 
 }
--- src/vuex/modules/userInfo.js
+++ src/vuex/modules/userInfo.js
@@ -3,56 +3,56 @@ import { storageUtils } from '../../common/utils/storageUtils';
 import { cookieUtils } from '../../common/utils/cookieUtils';
 import { config } from '../../config/config';
 const state = {
-    name: '',
-    phone: '',
+	name: '',
+	phone: '',
 	addressList: [],
 	selectAddress: null
 }
 const mutations = {
-	[types.SET_USERNAME] (state, { result }) {
+	[types.SET_USERNAME](state, { result }) {
 		state.name = result;
 		let oneDay = 24 * 3600;
-		cookieUtils.setCookie('userName',result,oneDay,config.domain);
+		cookieUtils.setCookie('userName', result, oneDay, config.domain);
 	},
-	[types.SET_USERPHONE] (state, { result }) {
+	[types.SET_USERPHONE](state, { result }) {
 		state.phone = result;
 		let oneDay = 24 * 3600;
-		cookieUtils.setCookie('userPhone',result,oneDay,config.domain);
+		cookieUtils.setCookie('userPhone', result, oneDay, config.domain);
 	},
-	[types.SET_USERADDRESSLIST] (state, { result }) {
+	[types.SET_USERADDRESSLIST](state, { result }) {
 		let userAddressList = cookieUtils.getCookie('userAddressList');
-		userAddressList ? state.addressList = userAddressList: state.addressList = [];
-		if(state.addressList.length > 0){
-			for(let i = 0 , len = state.addressList.length; i < len ; i++){
-				if(state.addressList[i].id == result.id){
+		userAddressList ? state.addressList = userAddressList : state.addressList = [];
+		if (state.addressList.length > 0) {
+			for (let i = 0, len = state.addressList.length; i < len; i++) {
+				if (state.addressList[i].id == result.id) {
 					state.addressList[i] = result;
 					break;
 				}
-				if(i == len -1){
+				if (i == len - 1) {
 					state.addressList.push(result);
 				}
 			}
-		}else{
+		} else {
 			state.addressList.push(result);
 		}
 		let oneDay = 24 * 3600;
-		cookieUtils.setCookie('userAddressList',state.addressList,oneDay,config.domain);
+		cookieUtils.setCookie('userAddressList', state.addressList, oneDay, config.domain);
 	},
-	[types.DELETE_USERADDRESSLIST] (state, { result }) {
-		for(let item of state.addressList){
-			if(item.id == result){
+	[types.DELETE_USERADDRESSLIST](state, { result }) {
+		for (let item of state.addressList) {
+			if (item.id == result) {
 				let index = state.addressList.indexOf(item);
-				state.addressList.splice(index,1);
+				state.addressList.splice(index, 1);
 			}
 		}
 		let oneDay = 24 * 3600;
-		cookieUtils.setCookie('userAddressList',state.addressList,oneDay,config.domain);
+		cookieUtils.setCookie('userAddressList', state.addressList, oneDay, config.domain);
 	},
-	[types.SET_USERSELECTADDRESS] (state, { result }) {
+	[types.SET_USERSELECTADDRESS](state, { result }) {
 		state.selectAddress = result;
-		storageUtils.setSessionStorage('userSelectAddress',result);
+		storageUtils.setSessionStorage('userSelectAddress', result);
 	}
-	
+
 }
 export default {
 	state,
